257c36e5c2e5e1d69386900f61c3317473d3fdd0
zhuwei-ones <zhuwei@ones.cn>
2023-01-17 15:09:23 +0800

    feat: standard add console test 2

--- packages/standard-version-demo/.versionrc.js
+++ packages/standard-version-demo/.versionrc.js
@@ -5,6 +5,9 @@ console.log();
 console.log();
 console.log();
 console.log();
+console.log();
+console.log();
+console.log();
 module.exports={
     lernaPackage:"standard-version-demo",
     tagPrefix:"standard-version-demo@",
