cf4cb1075fd709928f8cdf84a06cfacce0126453
zhuwei-ones <zhuwei@ones.cn>
2023-01-17 15:08:21 +0800

    feat: standard add tagPrefix

--- packages/standard-version-demo/.versionrc.js
+++ packages/standard-version-demo/.versionrc.js
@@ -6,6 +6,6 @@ console.log();
 console.log();
 module.exports={
     lernaPackage:"standard-version-demo",
-    tagPrefix:"standard-version-demo",
+    tagPrefix:"standard-version-demo@",
     path:"."
 }
