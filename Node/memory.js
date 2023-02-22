0289dcc4b215eccc9c9c196d5f6174303c639767
zhuwei-ones <zhuwei@ones.cn>
2023-02-22 11:16:10 +0800

    chore: node

---
+++ Node/memory.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+arr.reverse();
+const used = process.memoryUsage();
+for (let key in used) {
+  console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+}
+
+console.log(used);
